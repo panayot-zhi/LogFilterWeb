57ba2a62ddde075a2abd0df2ba95c81702584e00
Panayot Ivanov <panayot_zhi@abv.bg>
2021-01-21 11:29:26 +0200

    Implement service method for range.

--- Services/SmartUCFService.cs
+++ Services/SmartUCFService.cs
@@ -10,18 +10,39 @@ namespace LogFilterWeb.Services
 {
     public static class SmartUCFService
     {
-        public static IEnumerable<StopwatchFile> GetOverallStopwatchData(DateTime from, DateTime to)
+        public static IEnumerable<StopwatchRecord> GetStopwatchRecordsForRange(DateTime from, DateTime to)
         {
-            // TODO: Remove ToList here
             var smartUCFRoute = Constants.GetSmartUCFRoute(Constants.SmartUCFDefaultConfig);
-            var csvFiles = FilesHelper.GetFilesFromDirectory(new DirectoryInfo(smartUCFRoute), "[stopwatch]-lists.csv").ToList();
-            
+            var csvFiles = FilesHelper.GetFilesFromDirectory(new DirectoryInfo(smartUCFRoute), "[stopwatch]-lists.csv");
+
             var filesInRange = csvFiles.Where(x =>
                 FilesHelper.ToDateTime(x.Directory.Name) >= from.Date &&
                 FilesHelper.ToDateTime(x.Directory.Name) <= to.Date
-            ).ToList();
+            );
 
-            var stopwatchFiles = filesInRange.Select(FilesHelper.ReadStopWatchFile).ToList();
+            var stopwatchRecords = filesInRange.Select(FilesHelper.ReadStopWatchRecords)
+                .SelectMany(x => x);
+
+            /*var stopwatchRecords = new List<StopwatchRecord>();
+            foreach (var fileInfo in filesInRange)
+            {
+                stopwatchRecords.AddRange(FilesHelper.ReadStopWatchRecords(fileInfo.FullName));
+            }*/
+
+            return stopwatchRecords;
+        }
+
+        public static IEnumerable<StopwatchFile> GetStopwatchFilesForRange(DateTime from, DateTime to)
+        {
+            var smartUCFRoute = Constants.GetSmartUCFRoute(Constants.SmartUCFDefaultConfig);
+            var csvFiles = FilesHelper.GetFilesFromDirectory(new DirectoryInfo(smartUCFRoute), "[stopwatch]-lists.csv");
+
+            var filesInRange = csvFiles.Where(x =>
+                FilesHelper.ToDateTime(x.Directory.Name) >= from.Date &&
+                FilesHelper.ToDateTime(x.Directory.Name) <= to.Date
+            );
+
+            var stopwatchFiles = filesInRange.Select(FilesHelper.ReadStopWatchFile);
 
             return stopwatchFiles;
         }
