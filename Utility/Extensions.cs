6cd4bb4c91198d446b66f25c4127679e69f41040
Panayot Ivanov <panayot_zhi@abv.bg>
2021-01-27 11:12:43 +0200

    Delete cookie from response if it cannot be parsed.

--- Utility/Extensions.cs
+++ Utility/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,13 +13,26 @@ namespace LogFilterWeb.Utility
         public static T ReadCookie<T>(this ControllerBase controller, string key) where T : new()
         {
             var cookieData = controller.HttpContext.Request.Cookies[key];
+            var jsonSerializerSettings = new JsonSerializerSettings()
+            {
+                // DateTimeZoneHandling = DateTimeZoneHandling.RoundtripKind
+            };
 
             if (cookieData == null)
             {
                 return new T();
             }
 
-            return JsonConvert.DeserializeObject<T>(cookieData);
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(cookieData, jsonSerializerSettings);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred while trying to parse {key} cookie: {ex.Message}", ex);
+                controller.HttpContext.Response.Cookies.Delete(key);
+                return new T();
+            }
         }
     }
 }
