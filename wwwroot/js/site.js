670c18fead995dd0962092ed6a9aa6dab817d2f4
Panayot Ivanov <panayot_zhi@abv.bg>
2021-02-02 10:35:43 +0200

    Color shenanigans.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,15 +1,77 @@
 ﻿// Panayot Ivanov
 
-var chartColors = {
-    white: "#FFFFFF",
-    darkblue: "#1e3d60",
-    blue: "rgb(54, 162, 235)",
-    green: "rgb(75, 192, 192)",
-    grey: "rgb(201, 203, 207)",
-    orange: "rgb(255, 159, 64)",
-    purple: "rgb(153, 102, 255)",
-    red: "rgb(255, 99, 132)",
-    yellow: "rgb(255, 205, 86)"
+const availableColors = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#3366cc", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac", "#b77322", "#16d620", "#b91383", "#f4359e", "#9c5935", "#a9c413", "#2a778d", "#668d1c", "#bea413", "#0c5922", "#743411"];
+
+// TODO: Color shenanigans, remove
+
+const chartColors = {
+  white: "#FFFFFF",
+  darkblue: "#1e3d60",
+  blue: "rgb(54, 162, 235)",
+  green: "rgb(75, 192, 192)",
+  grey: "rgb(201, 203, 207)",
+  orange: "rgb(255, 159, 64)",
+  purple: "rgb(153, 102, 255)",
+  red: "rgb(255, 99, 132)",
+  yellow: "rgb(255, 205, 86)"
+}
+
+function hex(c) {
+    var s = "0123456789abcdef";
+    var i = parseInt(c);
+    if (i == 0 || isNaN(c))
+        return "00";
+    i = Math.round(Math.min(Math.max(0, i), 255));
+    return s.charAt((i - i % 16) / 16) + s.charAt(i % 16);
+}
+
+/* Convert an RGB triplet to a hex string */
+function convertToHex(rgb) {
+    return hex(rgb[0]) + hex(rgb[1]) + hex(rgb[2]);
+}
+
+/* Remove '#' in color hex string */
+function trim(s) { return (s.charAt(0) == '#') ? s.substring(1, 7) : s }
+
+/* Convert a hex string to an RGB triplet */
+function convertToRGB(hex) {
+    var color = [];
+    color[0] = parseInt((trim(hex)).substring(0, 2), 16);
+    color[1] = parseInt((trim(hex)).substring(2, 4), 16);
+    color[2] = parseInt((trim(hex)).substring(4, 6), 16);
+    return color;
+}
+
+function generateColors(colorStart, colorEnd, colorCount) {
+
+    // The beginning of your gradient
+    var start = convertToRGB(colorStart);
+
+    // The end of your gradient
+    var end = convertToRGB(colorEnd);
+
+    // The number of colors to compute
+    var len = colorCount;
+
+    //Alpha blending amount
+    var alpha = 0.0;
+
+    var colors = [];
+
+    for (i = 0; i < len; i++) {
+        var c = [];
+        alpha += (1.0 / len);
+
+        c[0] = start[0] * alpha + (1 - alpha) * end[0];
+        c[1] = start[1] * alpha + (1 - alpha) * end[1];
+        c[2] = start[2] * alpha + (1 - alpha) * end[2];
+
+        colors.push("#" + convertToHex(c));
+
+    }
+
+    return colors;
+
 }
 
 function drawBlueCanvasBackground(ctx) {
