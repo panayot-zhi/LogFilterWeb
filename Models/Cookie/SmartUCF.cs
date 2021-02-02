bcd792f47a97576eb68200eb609fe9c5549c70ef
Panayot Ivanov <panayot_zhi@abv.bg>
2021-02-02 16:13:17 +0200

    Clean up for now.

---
+++ Models/Cookie/SmartUCF.cs
@@ -0,0 +1,11 @@
+﻿using LogFilterWeb.Utility;
+
+namespace LogFilterWeb.Models.Cookie
+{
+    public class SmartUCF : DateRange
+    {
+        public static readonly string CookieName = "SmartUCFConfig";
+
+        public string[] MonitoredServers { get; set; } = Constants.SmartUCFMachines;
+    }
+}
